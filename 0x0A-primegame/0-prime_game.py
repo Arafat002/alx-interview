745a38a8326f97229e994a3cede4d80c73bdfeca
arafat motolani <arafatmotolani@yahoo.com>
2023-10-04 01:24:16 -0700

    fixed requirement

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,16 +6,16 @@ Define isWinner function, a solution to the Prime Game problem
 
 def primes(n):
     """Return list of prime numbers between 1 and n inclusive
-    Args:
-    n(int): upper boundary of range. lower boundary is alway 1
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
     """
     prime = []
     sieve = [True] * (n + 1)
     for p in range(2, n + 1):
         if (sieve[p]):
             prime.append(p)
-            for i in range(p, n + 1, p):\
-                    sieve[i] = False
+            for i in range(p, n + 1, p):
+                sieve[i] = False
     return prime
 
 
@@ -23,7 +23,7 @@ def isWinner(x, nums):
     """
     Determines winner of Prime Game
     Args:
-       x (int): no. of rounds of game
+        x (int): no. of rounds of game
         nums (int): upper limit of range for each round
     Return:
         Name of winner (Maria or Ben) or None if winner cannot be found
